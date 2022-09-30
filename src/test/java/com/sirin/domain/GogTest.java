0456e190d1ec2d124697b36f7fbf2457607eae66
AnastasiyaIlkevich <sirin199@yandex.ru>
2022-09-30 20:36:45 +0300

    test v2 (@BeforeEach)

--- src/test/java/com/sirin/domain/GogTest.java
+++ src/test/java/com/sirin/domain/GogTest.java
@@ -1,22 +1,27 @@
 package com.sirin.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GogTest {
 
+    Dog dog;
+
+    @BeforeEach
+    void paramData(){
+        dog = new Dog("sirin",2);
+    }
+
+
     @Test
     void getName() {
-
-        Dog dog = new Dog("sirin",2);
-
         assertEquals("sirin",dog.getName());
     }
 
     @Test
     void setName() {
-        Dog dog = new Dog("sirin",2);
         dog.setName("Kuzya");
 
         assertEquals("sirin",dog.getName());
